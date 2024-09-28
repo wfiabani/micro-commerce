2c6dc2955f59a5563df63cb8cd8fe74225604385
Wilian Fiabani <fiabani.wilian@gmail.com>
2024-09-28 11:39:44 -0300

    feat: Add JQuery, Bootstrap, SweetAlert2, Animate.css e Font Awesome and others front-end features

---
+++ src/main/resources/static/js/scripts.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    console.log("O DOM foi completamente carregado!");
+
+    // Aqui você pode adicionar outras ações a serem executadas
+    // quando o DOM estiver pronto
+});
