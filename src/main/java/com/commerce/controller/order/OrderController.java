4d12ea350ca4557986655b66ffaf157f72d7ba16
Wilian Fiabani <fiabani.wilian@gmail.com>
2024-10-17 18:05:28 -0300

    feat: Add Mercado Pago and config environment settings at application.yml

--- src/main/java/com/commerce/controller/order/OrderController.java
+++ src/main/java/com/commerce/controller/order/OrderController.java
@@ -4,11 +4,20 @@ import com.commerce.controller.cart.schema.CartMapper;
 import com.commerce.exception.ProductNotFoundException;
 import com.commerce.manager.CartManager;
 import com.commerce.model.session.Cart;
+import com.mercadopago.MercadoPagoConfig;
+import com.mercadopago.client.preference.PreferenceClient;
+import com.mercadopago.client.preference.PreferenceItemRequest;
+import com.mercadopago.client.preference.PreferenceRequest;
+import com.mercadopago.resources.preference.Preference;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("pedido")
@@ -17,13 +26,49 @@ public class OrderController {
 
     private final CartManager cartManager;
 
+    @Value("${custom.MP_PUBLIC_KEY}")
+    private String mpPublicKey;
+
+    @Value("${custom.MP_ACCESS_TOKEN}")
+    private String mpAccessToken;
+
     public OrderController(CartManager cartManager) {
         this.cartManager = cartManager;
     }
 
     @GetMapping("/detalhes-do-pedido")
     public String getOrderForm(Model model) {
+
         try {
+
+            /*********************************************************/
+            model.addAttribute("mpPublicKey", mpPublicKey);
+            MercadoPagoConfig.setAccessToken(mpAccessToken);
+
+            PreferenceItemRequest itemRequest =
+                    PreferenceItemRequest.builder()
+                            .id("1234")
+                            .title("Games")
+                            .description("PS5")
+                            .pictureUrl("http://picture.com/PS5")
+                            .categoryId("games")
+                            .quantity(2)
+                            .currencyId("BRL")
+                            .unitPrice(new BigDecimal("4000"))
+                            .build();
+            List<PreferenceItemRequest> items = new ArrayList<>();
+            items.add(itemRequest);
+            // Criando a solicitação de preferência
+            PreferenceRequest preferenceRequest = PreferenceRequest.builder()
+                    .items(items)
+                    .build();
+
+            // Criando o cliente de preferência e enviando a solicitação
+            PreferenceClient client = new PreferenceClient();
+            Preference preference = client.create(preferenceRequest);  // Usar
+            model.addAttribute("preferenceId", preference.getId());
+            /*********************************************************/
+
             Cart cart = cartManager.getCart();
             var response = CartMapper.INSTANCE.toGetCart(cart);
             model.addAttribute("cart", response);
@@ -32,7 +77,19 @@ public class OrderController {
         } catch (ProductNotFoundException e) {
             model.addAttribute("errorMessage", e.getMessage());
             return "error/error"; // Redireciona para uma página de erro customizada
+        } catch (Exception e) {
+            System.out.println(e.toString());
+            model.addAttribute("errorMessage", e.getMessage());
+            return "error/error"; // Redireciona para uma página de erro customizada
         }
+    }
+
+
+
+    @PostMapping("/webhooks")
+    @ResponseBody
+    public void handleWebhook(@RequestBody String payload) {
+        System.out.println("Recebido webhook: " + payload);
     }
 
 
