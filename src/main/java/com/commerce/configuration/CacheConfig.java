5d03fa6b1d30ddd9a04f71b29bc5722888d65bf6
Wilian Fiabani <fiabani.wilian@gmail.com>
2024-11-20 10:35:55 -0300

    feat: Add Caffeine for caching

---
+++ src/main/java/com/commerce/configuration/CacheConfig.java
@@ -0,0 +1,25 @@
+package com.commerce.configuration;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+public class CacheConfig {
+
+    @Bean
+    public CacheManager cacheManager() {
+        return new CaffeineCacheManager("products");
+    }
+
+    @Bean
+    public Caffeine<Object, Object> caffeineConfig() {
+        return Caffeine.newBuilder()
+                .expireAfterWrite(10, TimeUnit.MINUTES)
+                .maximumSize(100);
+    }
+}
--- src/main/java/com/commerce/manager/ProductManager.java
+++ src/main/java/com/commerce/manager/ProductManager.java
@@ -3,6 +3,7 @@ package com.commerce.manager;
 import com.commerce.exception.ProductNotFoundException;
 import com.commerce.model.Product;
 import com.commerce.repository.ProductRepository;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -18,6 +19,7 @@ public class ProductManager {
         this.productRepository = productRepository;
     }
 
+    @Cacheable("products")
     public Product getProduct(Long id) {
         Product product = productRepository.findById(id)
                 .orElseThrow(() -> new ProductNotFoundException("Product with ID " + id + " not found."));
@@ -26,6 +28,7 @@ public class ProductManager {
         return product;
     }
 
+    @Cacheable("products")
     public Page<Product> getProductsByCategory(Long categoryId, PageRequest pageRequest) {
         return productRepository.findAllByCategoryId(categoryId, pageRequest);
     }
